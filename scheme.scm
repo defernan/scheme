afca63472d320681ba3b79d1d0da17153f513299
Dezmon Fernandez <defernan@mines.edu>
2015-04-27 14:38:30 -0600

    added filter

--- scheme.scm
+++ scheme.scm
@@ -4,7 +4,17 @@
   (lambda(x)
     (* 2 x)))
 
-(map double '(2 3 4 5))
 (define doubleList
   (lambda(xs)
     (map double xs)))
+
+"Double list (1 2 3 4 5)"
+(doubleList '(1 2 3 4 5))
+
+;;filter example
+(define positiveList
+  (lambda(xs)
+    (filter positive? xs)))
+
+"Filter positives on list (-2 3 -4 5 -1 -3 8)"
+(positiveList '(-2 3 -4 5 -1 -3 8))
