2b2cb67cc116b69d8fb77660b2f448930690ef85
Dezmon Fernandez <defernan@mines.edu>
2015-04-27 15:29:13 -0600

    added lexical closure example

--- scheme.scm
+++ scheme.scm
@@ -1,4 +1,5 @@
 #lang scheme
+;;HIGHER ORDER EXAMPLES
 ;;map example
 (define double
   (lambda(x)
@@ -18,3 +19,16 @@
 
 "Filter positives on list (-2 3 -4 5 -1 -3 8)"
 (positiveList '(-2 3 -4 5 -1 -3 8))
+
+;;lexical closure example
+(define (create-account balance)
+  (lambda (n)
+    (set! balance (+ balance n))
+    balance))
+"create my account with 20, call (define my_account (create-account 20))"
+(define my_account (create-account 20))
+"call (my_account 20)"
+(my_account 20)
+"call (my_account -40)"
+(my_account -40)
+
